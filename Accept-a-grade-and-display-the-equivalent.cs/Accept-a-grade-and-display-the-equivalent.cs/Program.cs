57d1dce6b2810687c6679e2fdf1323ac2ddec906
Hari1982000 <89004693+Hari1982000@users.noreply.github.com>
2022-07-24 20:36:41 +0530

    Add files via upload

---
+++ Accept-a-grade-and-display-the-equivalent.cs/Accept-a-grade-and-display-the-equivalent.cs/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+class program
+{
+    public static void Main()
+    {
+        char grade;
+        Console.WriteLine("Enter the Grade in UpperCase \n");
+        grade = Convert.ToChar(Console.ReadLine());
+        switch (grade)
+        {
+            case 'E':
+                Console.WriteLine("Excellent");
+                break;
+            case 'V':
+                Console.WriteLine("Very Good");
+                break;
+            case 'G':
+                Console.WriteLine("Good");
+                break;
+            case 'A':
+                Console.WriteLine("Average");
+                break;
+            case 'F':
+                Console.WriteLine("Fail");
+                break;
+            default:
+                Console.WriteLine("ERROR IN GRADE \n");
+                break;
+                Console.ReadLine();
+        }
+    }
+}
